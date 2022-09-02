bbef714d5e7a08a69e1994335b59b69362125a01
yorqinjon <yorqinjonmahmudov93@gmail.com>
2022-09-02 06:55:51 +0500

    updating sorting test

--- src/main/java/com/epam/rd/autotasks/Sorting.java
+++ src/main/java/com/epam/rd/autotasks/Sorting.java
@@ -1,7 +1,9 @@
 package com.epam.rd.autotasks;
 
+import java.util.Arrays;
+
 public class Sorting {
     public void sort(int[] array){
-        throw new UnsupportedOperationException();
+        Arrays.sort(array);
     }
 }
--- src/main/java/com/epam/rd/autotasks/SortingTest.java
+++ src/main/java/com/epam/rd/autotasks/SortingTest.java
@@ -1,35 +1,48 @@
 package com.epam.rd.autotasks;
 
+import org.junit.Assert;
 import org.junit.Test;
+
 
 
 public class SortingTest {
 
     Sorting sorting = new Sorting();
 
-    @Test
-    public void testNullCase(){
-
+    @Test(expected = NullPointerException.class)
+    public void testNullCase() {
+        sorting.sort(null);
     }
 
     @Test
-    public void testEmptyCase(){
-
+    public void testEmptyCase() {
+        int[] array = {};
+        sorting.sort(array);
+        Assert.assertEquals(0, array.length);
     }
 
     @Test
     public void testSingleElementArrayCase() {
-
+        int[] array = {1};
+        int[] sortedArray = {1};
+        sorting.sort(array);
+        Assert.assertArrayEquals(sortedArray, array);
     }
 
     @Test
     public void testSortedArraysCase() {
-
+        int[] array = {1, 7, 5, 9, 2, 8, 3, 6, 4};
+        int[] sortedArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        sorting.sort(array);
+        Assert.assertArrayEquals(sortedArray, array);
     }
 
-    @Test
+    @Test(expected = AssertionError.class)
     public void testOtherCases() {
-
+        int[] array = {1, 7, 5, 9, 2, 8, 3, 6, 4};
+        int[] sortedArray = {1,1, 3, 4, 5, 6, 7, 8, 8,7,9};
+        sorting.sort(array);
+        Assert.assertArrayEquals(sortedArray, array);
     }
 
 }
